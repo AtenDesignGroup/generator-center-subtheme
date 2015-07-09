cfef3710a46bcd1653e18f2e19051def9ddd4989
John Ferris <john@pixel-whip.com>
2015-07-09 09:13:19 -0600

    Complete restructuring of the sass folder.

--- app/index.js
+++ app/index.js
@@ -29,16 +29,35 @@ var DrupalPrototypeGenerator = yeoman.generators.Base.extend({
         default: 'A custom theme implemtation for ' + _.startCase(this.appname) + '. Based on the Prototype starter theme.'
       },
       {
+        type: 'list',
+        name: 'packageManager',
+        message: 'How would you like to manage external packages?',
+        choices: [
+            'Eyeglass',
+            'Bower'
+          ],
+          default: 'Eyeglass',
+          filter: function(val) { return val.toLowerCase();
+        }
+      },
+      {
         type: 'confirm',
         name: 'useBreakpoint',
         message: 'Would you like to include Breakpoint Sass?',
         default: true
       },
       {
-        type: 'confirm',
-        name: 'useSusy',
-        message: 'Would you like to include the Susy Grid framework?',
-        default: true
+        type: 'list',
+        name: 'gridFramework',
+        message: 'Which grid framework would you like to use?',
+        choices: [
+            'Singularity',
+            'Susy',
+            'None'
+          ],
+          default: 'Singularity',
+          filter: function(val) { return val.toLowerCase();
+        }
       },
       {
         type: 'confirm',
@@ -52,8 +71,9 @@ var DrupalPrototypeGenerator = yeoman.generators.Base.extend({
       this.themeName = props.themeName;
       this.themeId = _.snakeCase(props.themeId);
       this.themeDesc = props.themeDesc;
+      this.packageManager = props.packageManager;
       this.useBreakpoint = props.useBreakpoint;
-      this.useSusy = props.useSusy;
+      this.gridFramework = props.gridFramework;
       this.useStyleguide = props.useStyleguide;
       done();
     }.bind(this));
@@ -89,27 +109,79 @@ var DrupalPrototypeGenerator = yeoman.generators.Base.extend({
   },
 
   installBower: function () {
-    if (this.options['skip-install']) return;
+    if (this.options['skip-install'] || this.packageManager !== 'bower') { return; }
 
     var options = {
-          'config.directory': 'src/bower',
-          'save': true,
-        },
-        components = [];
+        'config.directory': 'src/bower',
+        'save': true,
+      },
+      components = [];
 
     this.template('bower.json');
     this.copy('.bowerrc');
 
-    if (this.useBreakpoint) components.push('compass-breakpoint');
-    if (this.useSusy) components.push('susy');
+    if (this.useBreakpoint) {
+      components.push('compass-breakpoint');
+    }
 
-    if (components.length <= 0) return;
+    switch (this.gridFramework) {
+    case 'singularity':
+      components.push('singularity');
+      break;
+    case 'susy':
+      components.push('susy');
+      break;
+    }
+
+    if (components.length <= 0) { return; }
 
     this.bowerInstall(components, options);
   },
 
+  installNpmPackages: function () {
+    if (this.options['skip-install'] || this.packageManager !== 'eyeglass') { return; }
+
+    var exec = require('child_process').exec,
+        components = [];
+
+    components.push('eyeglass');
+
+    if (this.useBreakpoint) {
+      components.push('breakpoint-sass');
+    }
+
+    switch (this.gridFramework) {
+    case 'singularity':
+      components.push('singularitygs');
+      break;
+    case 'susy':
+      components.push('susy');
+      break;
+    }
+
+    function handleStd(error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    }
+
+    function installComponent(component) {
+      exec('npm install ' + component +' --save-dev', handleStd);
+    }
+
+    function installComponents(components) {
+      _.forEach(components, installComponent);
+    }
+
+    if (components.length <= 0) { return; }
+
+    installComponents(components);
+  },
+
   copyStyleguide: function () {
-    if (!this.useStyleguide) return;
+    if (!this.useStyleguide) { return; }
     this.directory('styleguide');
   }
 });
